0df31404cbdabf14acb86e6e1f1bd1ba3dac1715
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-04-20 12:40:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0.001
+const ethAmount = 0.08
 const image = 'https://images-ext-2.discordapp.net/external/5FI7neL0rIaD_xjYOh6NIyBP_G3sHewdKHOQ-rhmIRI/https/pbs.twimg.com/profile_images/1515763747574403075/RypnM0Ki_400x400.jpg'
 const Title = 'Better Bunnies'
 const supply = 3000
