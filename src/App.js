18a4e98d77e47a20202580be41c52ad964705b24
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-30 20:52:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.077'
-const image = 'https://pbs.twimg.com/profile_images/1620035744822661121/RDPtnTZR_400x400.jpg'
-const Title = 'MetaZeus NFT '
-const supply = 1810
+const ethAmount = '0.08'
+const image = 'https://pbs.twimg.com/profile_images/1624376454354960384/IaPhfzQf_400x400.jpg'
+const Title = 'Lil Birbs DAO'
+const supply = 1000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
