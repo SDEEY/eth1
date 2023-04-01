d9d1ee6d79a0e01f995587466c334873eba26c0e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-01 20:50:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.082 ETH / 135 MATIC'
-const image = 'https://pbs.twimg.com/media/FsclRKSacAASIW9?format=jpg&name=medium'
-const Title = 'FIT OFF'
-const supply = 22222
+const ethAmount = '0.0035'
+const image = 'https://pbs.twimg.com/profile_images/1624083849352192003/UvsxNqtG_400x400.jpg'
+const Title = 'United Hybrid Club'
+const supply = 150
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
