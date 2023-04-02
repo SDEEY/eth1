c419a133fb10a6d0419e950d6411009e64aa2277
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-02 22:56:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.0035'
-const image = 'https://pbs.twimg.com/profile_images/1624083849352192003/UvsxNqtG_400x400.jpg'
-const Title = 'United Hybrid Club'
-const supply = 150
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1611388522857906181/lIUUqp71_400x400.jpg'
+const Title = 'Andy Prinz'
+const supply = 1000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
