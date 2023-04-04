0c9e2b5c88e4db43584a521f50848f0ffd58cb11
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-05 00:44:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1611388522857906181/lIUUqp71_400x400.jpg'
-const Title = 'Andy Prinz'
-const supply = 1000
+const ethAmount = '0.046'
+const image = 'https://pbs.twimg.com/profile_images/1590388969019125761/Mr_rEVsn_400x400.jpg'
+const Title = 'SUPERMETA'
+const supply = 80
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
