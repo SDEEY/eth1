729a43e1635b7fe38992accbc2c68ddc0677da04
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-08 22:58:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1654475583084961793/-XMn8KJA_400x400.jpg'
-const Title = 'Las Vensus Girls'
-const supply = 5800
+const ethAmount = '1'
+const image = 'https://pbs.twimg.com/profile_images/1652022995928399898/A4EChajc_400x400.jpg'
+const Title = 'caSUIno'
+const supply = 2222
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
