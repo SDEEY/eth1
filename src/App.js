cf6faff77b10ddb869743269a2ad8b4feddf0174
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-17 16:51:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '350
+const ethAmount = '350'
 const image = 'https://pbs.twimg.com/profile_images/1597501678894436352/zejJW5VI_400x400.jpg'
 const Title = 'Legends of Elysium'
 const supply = 144
