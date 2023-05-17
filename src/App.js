822ae761f73615ba5293c1f105489c2ac575be11
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-17 16:48:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '3'
-const image = 'https://pbs.twimg.com/profile_images/1612526600444076053/k2JMccBN_400x400.jpg'
-const Title = 'Defina'
-const supply = 744
+const ethAmount = '350
+const image = 'https://pbs.twimg.com/profile_images/1597501678894436352/zejJW5VI_400x400.jpg'
+const Title = 'Legends of Elysium'
+const supply = 144
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
