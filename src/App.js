67489723519a708a31170782d01d9245057d3a65
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-19 19:54:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.01'
-const image = 'https://pbs.twimg.com/profile_images/1658252693473746947/w9Kfxvsk_400x400.jpg'
-const Title = 'The Sprawl'
-const supply = 890
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1520119872079998985/pBYe_HYv_400x400.jpg'
+const Title = 'PLANET MOJO'
+const supply = 3000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
