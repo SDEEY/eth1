9c4fcb34758b3ed5ce33e41ab74e6139ca35f335
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-06-14 17:54:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '2'
-const image = 'https://pbs.twimg.com/profile_images/1660320354693271552/1a2OKxOq_400x400.jpg'
-const Title = 'Verseye'
-const supply = 500
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1634935675312021506/Q-pwERLv_400x400.jpg'
+const Title = 'Polygon Pearls'
+const supply = 371
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
