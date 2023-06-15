c310c070353ce368acb44b289512e7ff67a289aa
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-06-15 19:11:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.02'
-const image = 'https://pbs.twimg.com/profile_images/1667193581306630155/07wPM0xn_400x400.jpg'
-const Title = 'Sitting Squirrels'
-const supply = 6000
+const ethAmount = '0.015/0.02'
+const image = 'https://pbs.twimg.com/profile_images/1660974012212584449/zs6b6SHK_400x400.jpg'
+const Title = 'The Working Dead'
+const supply = 4371
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
