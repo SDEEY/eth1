d3e8c441411c8241fdb011769bb3b0228b433e78
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-12-18 13:07:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.01'
-const image = 'https://pbs.twimg.com/profile_images/1721848556045193217/HLsTgeJr_400x400.jpg'
-const Title = 'ANT REPUBLIC'
-const supply = 1844
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1690997775784443904/GDf5WOJB_400x400.jpg'
+const Title = 'Kentaro'
+const supply = 500
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
